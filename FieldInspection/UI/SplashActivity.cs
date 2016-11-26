d142a45e61126bf97690a07fd149304305c8dc3c
walcoot <andreisofronee@gmail.com>
2016-11-26 21:56:37 +0200

    Added NavigationDrawer

--- FieldInspection/UI/SplashActivity.cs
+++ FieldInspection/UI/SplashActivity.cs
@@ -5,15 +5,15 @@ using Android.OS;
 
 namespace FieldInspection
 {
-    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
-    public class SplashActivity : Activity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-            Thread.Sleep(2500);
-            StartActivity(typeof(MainActivity));
-        }
-    }
+	[Activity(Theme = "@style/Theme.Splash", NoHistory = true)]
+	public class SplashActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			Thread.Sleep(2500);
+			StartActivity(typeof(MainActivity));
+		}
+	}
 }
 
