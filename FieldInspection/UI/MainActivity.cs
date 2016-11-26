8b22ebea3a7b3d547f6b59832a62527b6b66659f
walcoot <andreisofronee@gmail.com>
2016-11-26 18:15:06 +0200

    MacTest

--- FieldInspection/UI/MainActivity.cs
+++ FieldInspection/UI/MainActivity.cs
@@ -12,7 +12,7 @@ namespace FieldInspection
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            // SetContentView sdf(Resource.Layout.Main);
         }
     }
 }
