5d236ce302765526877a53cc66d19c0c058c352b
andreis <andreis@P-3408>
2016-12-09 14:53:47 +0200

    Added camera functionality

--- FieldInspection/UI/MainActivity.cs
+++ FieldInspection/UI/MainActivity.cs
@@ -1,28 +1,68 @@
-﻿using Android.Widget;
-using Android.OS;
-using Android.Support.Design.Widget;
+﻿using Android.Support.Design.Widget;
 using Android.Support.V4.Widget;
 using Android.Support.V7.App;
 using Android.Views;
-using Android.App;
-using System.Threading.Tasks;
-using System.Net;
-using System;
-using System.IO;
 using System.Json;
-using Xamarin.Android;
 
 
 namespace FieldInspection
+
 {
-	[Activity(Label = "FieldInspection", Theme = "@style/MyTheme.Base", Icon = "@drawable/icon")]
+    using System;
+    using System.Collections.Generic;
+    using Android.App;
+    using Android.Content;
+    using Android.Content.PM;
+    using Android.Graphics;
+    using Android.OS;
+    using Android.Provider;
+    using Android.Widget;
+    using Java.IO;
+    using Environment = Android.OS.Environment;
+    using Uri = Android.Net.Uri;
+
+    public static class App
+    {
+        public static File _file;
+        public static File _dir;
+        public static Bitmap bitmap;
+    }
+    [Activity(Label = "FieldInspection", Theme = "@style/MyTheme.Base", Icon = "@drawable/icon")]
+
 	public class MainActivity : AppCompatActivity
 	{
 		DrawerLayout drawerLayout;
-		protected override void OnCreate(Bundle savedInstanceState)
+        private ImageView _imageView;
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            // Make it available in the gallery
+
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            Uri contentUri = Uri.FromFile(App._file);
+            mediaScanIntent.SetData(contentUri);
+            SendBroadcast(mediaScanIntent);
+
+            // Display in ImageView. We will resize the bitmap to fit the display.
+            // Loading the full sized image will consume to much memory
+            // and cause the application to crash.
+
+            int height = Resources.DisplayMetrics.HeightPixels;
+            int width =  _imageView.Height;
+            App.bitmap = App._file.Path.LoadAndResizeBitmap(width, height);
+            if (App.bitmap != null)
+            {
+                _imageView.SetImageBitmap(App.bitmap);
+                App.bitmap = null;
+            }
+
+            // Dispose of the Java side bitmap.
+            GC.Collect();
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
 		{
-
-
 			SetContentView(Resource.Layout.Main);	
 			base.OnCreate(savedInstanceState);
 
@@ -52,30 +92,66 @@ namespace FieldInspection
 			ft.AddToBackStack(null);
 			ft.Add(Resource.Id.HomeFrameLayout, new HomeFragment());
 			ft.Commit();
-		}
 
-		private async Task<JsonValue> FetchWeatherAsync(string url)
-		{
-			// Create an HTTP web request using the URL:
-			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
-			request.ContentType = "application/json";
-			request.Method = "GET";
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
 
-			// Send the request to the server and wait for the response:
-			using (WebResponse response = await request.GetResponseAsync())
-			{
-				// Get a stream representation of the HTTP web response:
-				using (Stream stream = response.GetResponseStream())
-				{
-					// Use this stream to build a JSON document object:
-					JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
-					Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+                _imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+                button.Click += TakeAPicture;
+            }
+        }
 
-					// Return the JSON document:
-					return jsonDoc;
-				}
-			}
-		}
+        private void TakeAPicture(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            App._file = new File(App._dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+            intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(App._file));
+            StartActivityForResult(intent, 0);
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            App._dir = new File(
+                Environment.GetExternalStoragePublicDirectory(
+                    Environment.DirectoryPictures), "CameraAppDemo");
+            if (!App._dir.Exists())
+            {
+                App._dir.Mkdirs();
+            }
+        }
+
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities =
+                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+  //      private async Task<JsonValue> FetchWeatherAsync(string url)
+		//{
+		//	// Create an HTTP web request using the URL:
+		//	HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+		//	request.ContentType = "application/json";
+		//	request.Method = "GET";
+
+		//	// Send the request to the server and wait for the response:
+		//	using (WebResponse response = await request.GetResponseAsync())
+		//	{
+		//		// Get a stream representation of the HTTP web response:
+		//		using (Stream stream = response.GetResponseStream())
+		//		{
+		//			// Use this stream to build a JSON document object:
+		//			JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+		//			Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+		//			// Return the JSON document:
+		//			return jsonDoc;
+		//		}
+		//	}
+		//}
 		private void ParseAndDisplay(JsonValue json)
 		{
 			// Get the weather reporting fields from the layout resource:
